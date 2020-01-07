42a2790faae30a4a6281eedd5cc4402581330283
Alexandr-Gusev <34835129+Alexandr-Gusev@users.noreply.github.com>
2020-01-07 16:33:54 +0300

    1

--- src/js/themes/index.js
+++ src/js/themes/index.js
@@ -3,10 +3,10 @@ import {createMuiTheme} from "@material-ui/core/styles";
 export const mainTheme = createMuiTheme({
 	palette: {
 		primary: {
-			main: "#468DBC",
+			main: "#468DBC"
 		},
 		secondary: {
-			main: "#C6453A",
+			main: "#C6453A"
 		}
 	}
 });
